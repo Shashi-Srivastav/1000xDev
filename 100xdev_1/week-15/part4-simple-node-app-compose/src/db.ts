e6b25044d736180fa887a0ee483f6dd9f89c56b6
Shashi Srivastava <Shashisrivastav96@gmail.com>
2024-07-23 16:35:22 +0530

    patym

--- 100xdev_1/week-15/part4-simple-node-app-compose/src/db.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const courseSchema = new mongoose.Schema({
-  title: String,
-  description: String
-});
-
-export const Course = mongoose.model('Course', courseSchema);
-
--- 100xdev_2/paytm/backend/index.js
+++ 100xdev_2/paytm/backend/index.js
@@ -1,40 +1,13 @@
-const express = require("express");
-const mongoose = require('mongoose');
-const zod = require('zod');
-const jwt = require('jsonwebtoken');
+// backend/index.js
+const express = require('express');
+const cors = require("cors");
+const rootRouter = require("./routes/index");
+
 const app = express();
-const port = 3000;
+
+app.use(cors());
 app.use(express.json());
-mongoose.connect(' mongodb://localhost:27017')
-const model = mongoose.model('patym',{
-    name:"String",
-    price:"Number",
-})
-app.get('/',(req,res)=>{
-    res.json({
-        msg:"Woking"
-    })
-})
-app.post('/signup',(req,res)=>{
-    const firstname = req.body.firstname;
-    const lastname = req.body.lastname;
-    const pass = req.body.pass;
-    const token = jwt.sign({user:firstname},"bittu");
-    res.json({msg:token});
-})
-app.get('/signin',(req,res)=>{
-    const token = req.headers.token;
-    const pass = req.headers.secret;
-    const verify = jwt.verify(token,pass);
-    res.json({msg:"success"})
-})
-app.put('/update',(req,res)=>{
-    const newname = req.body.newname;
-    const newlastname = req.body.newlastname;
-    const newpass = req.body.newpass;
 
-})
-app.listen(port,()=>{
-    console.log(`http://localhost:${port}`);
-})
+app.use("/api/v1", rootRouter);
 
+app.listen(3000);
